e45101f18402c049790b7ac4cf6fa5fb1014023f
Konstantinos Maninakis <maninak@protonmail.com>
2023-12-15 16:30:21 +0200

    chore(webview): config basic linter
    Signed-off-by: Konstantinos Maninakis <maninak@protonmail.com>

--- src/webviews/src/App.vue
+++ src/webviews/src/App.vue
@@ -2,10 +2,6 @@
 import Counter from '@/components/Counter.vue'
 import { registerWebviewMessageHandlers } from '@/composables/registerWebviewMessageHandlers'
 
-// TODO: maninak add linter and hook-up scripts
-// TODO: maninak add component auto-import
-// TODO: maninak add auto-import vue-use
-
 registerWebviewMessageHandlers()
 </script>
 
