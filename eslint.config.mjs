346185f42d9a4246770d4e7c4c04beba7da158c0
Konstantinos Maninakis <maninak@protonmail.com>
2026-06-16 07:21:38 +0200

    refactor(repo): remove leftover references to outdated dir
    Signed-off-by: Konstantinos Maninakis <maninak@protonmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,5 @@ import maninak from '@maninak/eslint-config'
 export default maninak({
   typescript: { tsconfigPath: ['./tsconfig.json', './test/e2e/tsconfig.wdio.json'] },
   requireJsdocInUtils: true,
-  ignores: ['src/webviews', 'static', 'dist', '.vscode-test'],
+  ignores: ['src/webviews', 'static', 'dist'],
 })
